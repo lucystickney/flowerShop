890128cabc070682d088b96f91d35cdab95e566e
Lucy Stickney <111818976+lucystickney@users.noreply.github.com>
2024-08-12 13:58:04 -0500

    moving handleGrowOrRemove to stats component

--- flower_shop/src/Stats.js
+++ flower_shop/src/Stats.js
@@ -1,27 +1,51 @@
+//import React, { useState, useEffect } from 'react';
 
-
-function Stats( {pot} ) {
+function Stats( {id, pot, showing, potState, changePot, setGameState, pollinate, growPlant} ) {
     const statement = (potState === 'grown') ? 'Remove plant' : 'Grow Plant';
+    console.log("pot: "+pot);
 
+    // const handleButtonClick = () => {
+    //     if (pot.state === 'grown') {
+    //         // the button should be remove
+    //         // change the state of the pot
+    //         // changing the state shouls change the image from useEffect in Pea
+    //     }   
+    // }
 
-    const handleButtonClick = () => {
-        if (pot.state === 'grown') {
-            // the button should be remove
-            // change the state of the pot
-            // changing the state shouls change the image from useEffect in Pea
-        }   
+    // click the button --> its either grow or remove
+    const handleGrowOrRemove = () => {
+        if (potState === 'grown') {
+            console.log('removing plant');
+            // remove the plant
+            changePot(id, 'state', 'empty');
+            changePot(id, 'color', '');
+            // update the pot's fields
+            
+            
+        } else {
+            // grow plant --> go into logic of cross polination
+            console.log('growing plant');
+            changePot(id, 'state', 'growing');
+            setGameState('pollinating');
+        }
     }
 
 
     return (
-        <div>
-            {showStats && (
+        <div className="stats-box">
+            {showing && ( 
             <div>
-                <h1>Pea Plant</h1>
+                
                 <p>Flower Color: {pot.color}</p>
-                <button>{statement}</button>
+                <button onClick={handleGrowOrRemove}>{statement}</button>
+                {pollinate &&  (
+                <button onClick={growPlant}>Grow New Plant</button>
+                )}
             </div>
             )}
         </div>
     )
+
 }
+
+export default Stats;
